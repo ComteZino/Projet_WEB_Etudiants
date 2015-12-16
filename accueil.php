f62ff983e19d70b16f587b5065375c72330793cb
sio2 <sio2@A3108>
2015-12-16 16:33:16 +0100

    Ajout image

---
+++ accueil.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <header>
+            <!-- Entête de la zone considérée -->
+            
+        </header>
+        <nav><!-- Nav. principale de la page -> site --></nav>        
+        <aside><!-- Les à-cotés de la page --></aside>
+        <article><!-- Contenu textuel de la page --></article>
+        <footer><!-- Pied-de-page de la page -> site --></footer>
+    </body>
+</html>
