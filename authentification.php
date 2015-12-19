af6f2e591b5a3a64091a329f6698bf0b2e8c05cd
Steven <Steven@192.168.1.55>
2015-12-19 22:46:40 +0100

    Indentation de toutes les pages php

--- authentification.php
+++ authentification.php
@@ -1,37 +1,35 @@
 <?php 
-    session_start(); 
+    session_start();
     $_SESSION['statut']="0";
 ?>
 <html>
     <head>
-      <meta charset="utf-8">
-      <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-      <title></title>
-      <link rel="stylesheet" href="css/styleCo.css">
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title></title>
+        <link rel="stylesheet" href="css/styleCo.css">
     </head>
     <body>
-    <header>
-        <!-- Entête de la zone considérée -->
-        <a>
-            <img src="images/logostpaul.png" alt="logo" title="logo" id="logo" /> 
-        </a>
-    </header>
+        <header>
+            <!-- Entête de la zone considérée -->
+            <a>
+                <img src="images/logostpaul.png" alt="logo" title="logo" id="logo" /> 
+            </a>
+        </header>
+        <form method="post" action="authentificationBD.php" class="login">
+            <p>
+                <label for="login">Identifiant :</label>
+                <input type="text" name="Identifiant" id="login" value="">
+            </p>
 
-      <form method="post" action="authentificationBD.php" class="login">
-        <p>
-          <label for="login">Identifiant :</label>
-          <input type="text" name="Identifiant" id="login" value="">
-        </p>
+            <p>
+                <label for="password">Password:</label>
+                <input type="password" name="Password" id="password" value="">
+            </p>
 
-        <p>
-          <label for="password">Password:</label>
-          <input type="password" name="Password" id="password" value="">
-        </p>
-
-        <p class="login-submit">
-          <button type="submit" class="login-button">Authentification</button>
-        </p>
-
-      </form>
+            <p class="login-submit">
+                <button type="submit" class="login-button">Authentification</button>
+            </p>
+        </form>
     </body>
 </html>
--- header.php
+++ header.php
@@ -1,17 +1,17 @@
 <?php 
-   if(!empty($_SESSION)) {
+   if(!empty($_SESSION)) 
+    {
         if($_SESSION['statut']=="0")
 	{
             header('Location: authentification.php');
-            
 	}
     }
-    else {
+    else 
+    {
         session_start(); 
-         if($_SESSION['statut']=="0" or $_SESSION['statut']=="Admin" or $_SESSION['statut']=="Util"   )
+        if($_SESSION['statut']=="0" or $_SESSION['statut']=="Admin" or $_SESSION['statut']=="Util")
 	{
             header('Location: authentification.php');
-            
 	}
     }
 ?>
