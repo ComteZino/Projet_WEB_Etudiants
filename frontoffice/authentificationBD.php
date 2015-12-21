f3a2b5e7afa319572c5394fd696984f156adcdf1
Steven <Steven@Steven-PC>
2015-12-21 17:53:55 +0100

    Création dossier frontoffice pour les pages visible par les utilisateurs

---
+++ frontoffice/authentificationBD.php
@@ -0,0 +1,37 @@
+<?php
+    if ($_POST)
+    {
+        $pseudo = htmlentities($_POST["Identifiant"]);
+        $mdp = htmlentities($_POST["Password"]);
+    }
+    else
+    {
+	$pseudo = htmlentities($_GET["Identifiant"]);
+	$mdp = htmlentities($_GET["Password"]);
+    }
+	require_once('connexionBD.php');
+        $mdp=md5($mdp);
+	$tableuser="SELECT * FROM compte WHERE login='".addslashes($pseudo)."' AND password='".addslashes($mdp)."'";
+        
+	$table = $connexion->query($tableuser);
+	$ligne = $table->fetch();
+        $statut=$ligne['statut'];
+        $_SESSION['statut'] = $statut;
+       
+	if($pseudo==$ligne['login'] and $mdp==$ligne['password'])
+	{	
+            if($statut=='Admin')
+            {
+                header('Location: accueil.php');    
+            }
+            else
+            {
+                header('Location: accueil.html');  
+            }
+	}
+	else
+	{
+            header('Location: authentification.php');    
+	}
+	$table->closeCursor();
+?>
---
+++ frontoffice/contact.php
@@ -0,0 +1,41 @@
+<?php 
+    session_start();
+    if(empty($_SESSION['statut'])) 
+    {
+        header('Location: authentification.php');
+    }
+?>
+<html>
+    <head>
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+      <title></title>
+      <link rel="stylesheet" href="css/style.css">
+      <link rel="stylesheet" href="css/styleContact.css">
+    </head>
+    <body>
+        <?php include 'header.php'; ?>
+        <?php include 'menu.php'; ?>    
+        <div id="form-main">
+            <div id="form-div">
+                <p class="name">
+                  <input name="name" type="text" class="validate[required,custom[onlyLetter],length[0,100]] feedback-input" placeholder="Nom" id="name" />
+                </p>
+
+                <p class="email">
+                  <input name="email" type="text" class="validate[required,custom[email]] feedback-input" id="email" placeholder="Email" />
+                </p>
+
+                <p class="text">
+                  <textarea name="text" class="validate[required,length[6,300]] feedback-input" id="comment" placeholder="Commentaire"></textarea>
+                </p>
+
+
+                <div class="submit">
+                  <input type="submit" value="Envoyer" id="button-blue"/>
+                  <div class="ease"></div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
