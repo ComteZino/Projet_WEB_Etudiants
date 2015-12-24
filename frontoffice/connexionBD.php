25f2f701d082130632662b221d0ba7990ee560b8
Steven <Steven@Steven-PC>
2015-12-24 19:26:49 +0100

    Indentation

--- frontoffice/connexionBD.php
+++ frontoffice/connexionBD.php
@@ -1,18 +1,19 @@
 <?php
-    if(isset($_SESSION)){
-		 if(empty($_SESSION['statut'])) 
-		{
-			header('Location: authentification.php');
-		}
-	}
-	else
-	{
-		session_start();
-		 if(empty($_SESSION['statut'])) 
-		{
-			header('Location: authentification.php');
-		}
-	}
+    if(isset($_SESSION))
+    {
+        if(empty($_SESSION['statut'])) 
+        {
+            header('Location: authentification.php');
+        }
+    }
+    else
+    {
+        session_start();
+        if(empty($_SESSION['statut'])) 
+        {
+            header('Location: authentification.php');
+        }
+    }
     try
     {
         $dns ='mysql:host=localhost;dbname=anciens_etudiants';
