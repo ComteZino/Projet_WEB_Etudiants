0abd60eda5ae5018e823e14f8332d213590abc7f
sio2 <sio2@A3107>
2016-01-06 16:27:59 +0100

    affichage des news sur l'accueil

--- frontoffice/accueil.php
+++ frontoffice/accueil.php
@@ -4,18 +4,65 @@
     {
         header('Location: authentification.php');
     }
+    require_once('connexionBD.php');
+    $tableNewsLycee="SELECT * FROM news WHERE categorie='lycee' ORDER BY `date` desc";     
+    $table = $connexion->query($tableNewsLycee);
+    $ligne = $table->fetch();
+    $auteurLycee=$ligne['auteur'];
+    $titreLycee=$ligne['titre'];
+    $contenuLycee=$ligne['contenu'];
+    $dateLycee=$ligne['date'];
+    
+    $tableNewsMariage="SELECT * FROM news WHERE categorie='mariage' ORDER BY `date` desc";     
+    $table1 = $connexion->query($tableNewsMariage);
+    $ligne1 = $table1->fetch();
+    $auteurMariage=$ligne1['auteur'];
+    $titreMariage=$ligne1['titre'];
+    $contenuMariage=$ligne1['contenu'];
+    $dateMariage=$ligne1['date'];
+    
+    $tableNewsDeces="SELECT * FROM news WHERE categorie='deces' ORDER BY `date` desc";     
+    $table2 = $connexion->query($tableNewsDeces);
+    $ligne2 = $table2->fetch();
+    $auteurDeces=$ligne2['auteur'];
+    $titreDeces=$ligne2['titre'];
+    $contenuDeces=$ligne2['contenu'];
+    $dateDeces=$ligne2['date'];
+
 ?>
 <html>
     <head>
         <meta charset="UTF-8">
         <link rel="stylesheet" href="../css/style.css" />
+        <link rel="stylesheet" href="../css/styleAccueil.css" />
         <title></title>
     </head>
     <body>
         <?php include 'header.php'; ?>
         <?php include 'menu.php'; ?>      
-        <aside><!-- Les à-cotés de la page --></aside>
-        <article><!-- Contenu textuel de la page --></article>
-        <footer><!-- Pied-de-page de la page -> site --></footer>
+        <div id="divPrincipal" class="form-style-5">        
+            <fieldset>
+                <legend><span class="number" id="number1">1</span>Dernière actualité du lycée :</legend>
+                <p><?php echo 'Auteur : '.$auteurLycee.' Date de publication : '.$dateLycee; ?></p>
+                <p><?php echo $titreLycee; ?></p>
+                <p><?php echo substr(htmlspecialchars_decode(strip_tags($contenuLycee)),0,140).'...'; ?></p>
+            </fieldset>        
+        </div>
+        <div id="divPrincipal" class="form-style-5">
+            <fieldset>
+                <legend><span class="number" id="number2">2</span>Dernière actualité de Mariage :</legend>
+                <p><?php echo 'Auteur : '.$auteurMariage.' Date de publication : '.$dateMariage; ?></p>
+                <p><?php echo $titreMariage; ?></p>
+                <p><?php echo substr(htmlspecialchars_decode(strip_tags($contenuMariage)),0,140).'...'; ?></p>
+            </fieldset>
+        </div>
+        <div id="divPrincipal" class="form-style-5">
+             <fieldset>
+                <legend><span class="number" id="number3">3</span>Dernière actualité de décès :</legend>
+                <p><?php echo 'Auteur : '.$auteurDeces.' Date de publication : '.$dateDeces; ?></p>
+                <p><?php echo $titreDeces; ?></p>
+                <p><?php echo substr(htmlspecialchars_decode(strip_tags($contenuDeces)),0,140).'...'; ?></p>
+            </fieldset>
+        </div>
     </body>
 </html>
