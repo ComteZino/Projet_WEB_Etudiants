e9c84d2ba22913e5bc5cb38b4fc44534f631d712
sio2 <sio2@A3109>
2016-01-20 13:25:00 +0100

    Ajout de l'onglet catégorie dans l'onglet gestion

--- backoffice/menu.php
+++ backoffice/menu.php
@@ -38,6 +38,7 @@
                             echo "<ul>";
                             echo "<li><a href='choix_gestion_compte.php' <span>Comptes</span></a></li>";
                             echo "<li><a href='choix_gestion_actualite.php' <span>Actualités</span></a></li>";
+                            echo "<li><a href='choix_gestion_categorie.php' <span>Catégories</span></a></li>";
                             echo "</ul></li>";
                         }
                         
